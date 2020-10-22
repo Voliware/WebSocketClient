0953ddf525031b8dff9d29bda60661a68ea631f1
tones31 <tones31@users.noreply.github.com>
2020-10-22 00:24:03 -0400

    2.1.1
    - EventSystem now built in to dist file as renamed class

--- build.js
+++ build.js
@@ -1,10 +1,16 @@
-const Build = require('@voliware/node-build').Build;
+const NodeBuild = require('@voliware/node-build');
 const version = require('./package.json').version;
 
-new Build({
+new NodeBuild.Build({
     name: "WebSocketClient",
     version: version,
-    input: "./lib/webSocketClient.js",
+    input: [
+        "./node_modules/@voliware/eventsystem/lib/eventSystem.js", 
+        "./lib/webSocketClient.js"
+    ],
     output: "./dist/webSocketClient.min.js",
-    minify: true
+    minify: true,
+    modifiers: [
+        new NodeBuild.Modifier("replace", "EventSystem", {string: "__WebSocketClientEventSystem__"})
+    ]
 }).run();
